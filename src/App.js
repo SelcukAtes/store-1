833f2bd54678f1abc6c87844747d94812bb5e517
SelcukAtes <sselcukates@gmail.com>
2019-05-04 14:32:36 -0700

    start working on AllProducts component to render all products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import Login from './components/Login.jsx'
+import Login from './components/Login.jsx';
+import AllProducts from './Container/AllProducts';
 
 function App() {
-  return (
-   <Login /> 
-  );
+	return (
+		<React.Fragment>
+			<Login />
+			<AllProducts />
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/Container/AllProducts.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AllProducts extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {}
+	render() {
+		return (
+			<React.Fragment>
+				<p>Hello From AllProducts Component </p>
+			</React.Fragment>
+		);
+	}
+}
+export default AllProducts;
