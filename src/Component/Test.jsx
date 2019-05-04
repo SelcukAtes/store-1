aa311e3da846fb964c65353572a4fffac6e7028c
SelcukAtes <sselcukates@gmail.com>
2019-05-04 11:07:13 -0700

    just tesing

---
+++ src/Component/Test.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class Test extends Component {
+	state = {
+		userName: '',
+		password: ''
+	};
+
+	render() {
+		return (
+			<div>
+				<p>just delete</p>
+			</div>
+		);
+	}
+}
+
+export default Test;
